338f858aaf3f14c939ffd9d9a2e4993acc51a013
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-12-02 12:39:40 +0100

    Update version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.xm2vts',
-    version='1.2.1',
+    version='1.2.2a0',
     description='XM2VTS Database Access API for Bob',
     url='https://pypi.python.org/pypi/xbob.db.xm2vts',
     license='GPLv3',
